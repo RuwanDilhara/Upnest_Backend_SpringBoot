705b89dcedd4f8cacc32a117af5ea6aa3956f8b7
RuwanDilhara <roshansameera916@gmail.com>
2025-04-21 14:59:40 +0530

    Created the PostEntity class

---
+++ src/main/java/org/icet/upnest/entity/PostEntity.java
@@ -0,0 +1,33 @@
+package org.icet.upnest.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.icet.upnest.dto.User;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "posts")
+@Entity
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String mediaURL;
+
+    @Column(nullable = false)
+    private LocalDate createAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserEntity user;
+
+}
