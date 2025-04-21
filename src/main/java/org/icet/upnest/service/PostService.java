f04343577f01221d08b412658227e49739bcdecb
RuwanDilhara <roshansameera916@gmail.com>
2025-04-21 14:58:05 +0530

    Created the PostService interface

---
+++ src/main/java/org/icet/upnest/service/PostService.java
@@ -0,0 +1,16 @@
+package org.icet.upnest.service;
+
+import org.icet.upnest.dto.Post;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PostService {
+    List<Post> getAll();
+    Post getPostByPostId(Integer id);
+    List<Post> getPostByUserId(Integer userId);
+    List<Post> getPostByCreateDate(LocalDate date);
+    Post save(Post post);
+    Post updatePost(Post post);
+    void deletePostById(Integer id);
+}
