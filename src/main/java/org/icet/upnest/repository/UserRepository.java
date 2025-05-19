16448861e170b96a367d8c9f77e815e57e0b6005
RuwanDilhara <roshansameera916@gmail.com>
2025-05-19 16:53:27 +0530

    removed the JWT & Google Login

--- src/main/java/org/icet/upnest/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.icet.upnest.repository;
-
-import org.icet.upnest.dto.User;
-import org.icet.upnest.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity,Integer> {
-    UserEntity findByUsername(String username);
-}
