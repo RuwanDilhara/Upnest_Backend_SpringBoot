84b6e316241cf78f5b11c1b1162d27d1184ebc40
RuwanDilhara <roshansameera916@gmail.com>
2025-05-19 16:47:57 +0530

    updated the PostRepository interface

--- src/main/java/org/icet/upnest/repository/feed/PostRepository.java
+++ src/main/java/org/icet/upnest/repository/feed/PostRepository.java
@@ -1,12 +1,15 @@
-package org.icet.upnest.repository;
+package org.icet.upnest.repository.feed;
 
 import org.icet.upnest.dto.Post;
 import org.icet.upnest.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity ,Integer> {
     List<PostEntity> findByUserId(Integer userId);
+
+    List<PostEntity> findByCreateAt(LocalDate date);
 }
