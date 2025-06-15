9cb0b9b65906d8affcb67f9bff5b9d318fa12e65
RuwanDilhara <roshansameera916@gmail.com>
2025-06-15 21:45:21 +0530

    updated the PostServiceImpl  class

--- src/main/java/org/icet/upnest/service/impl/PostServiceImpl.java
+++ src/main/java/org/icet/upnest/service/impl/PostServiceImpl.java
@@ -1,8 +1,8 @@
 package org.icet.upnest.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.icet.upnest.dto.Post;
-import org.icet.upnest.entity.PostEntity;
+import org.icet.upnest.dto.feed.PostDTO;
+import org.icet.upnest.entity.feed.Post;
 import org.icet.upnest.repository.feed.PostRepository;
 import org.icet.upnest.service.PostService;
 import org.modelmapper.ModelMapper;
@@ -18,14 +18,14 @@ public class PostServiceImpl implements PostService {
     final ModelMapper mapper;
 
     @Override
-    public List<Post> getAll() {
-        return postRepository.findAll().stream().map(postEntity ->
-                mapper.map(postEntity, Post.class)).toList();
+    public List<PostDTO> getAll() {
+        return postRepository.findAll().stream().map(post ->
+                mapper.map(post, PostDTO.class)).toList();
     }
 
     @Override
-    public Post getPostByPostId(Integer id) {
-        return mapper.map(postRepository.findById(id).orElse(null), Post.class);
+    public PostDTO getPostByPostId(Integer id) {
+        return mapper.map(postRepository.findById(id).orElse(null), PostDTO.class);
     }
 
 //    @Override
@@ -35,21 +35,21 @@ public class PostServiceImpl implements PostService {
 //    }
 
     @Override
-    public List<Post> getPostByCreateDate(LocalDate date) {
-        return postRepository.findByCreateAt(date).stream().map(postEntity ->
-                mapper.map(postEntity,Post.class)).toList();
+    public List<PostDTO> getPostByCreateDate(LocalDate date) {
+        return postRepository.findByCreateAt(date).stream().map(post ->
+                mapper.map(post, PostDTO.class)).toList();
     }
 
     @Override
-    public Post save(Post post) {
+    public PostDTO save(PostDTO post) {
         return  mapper.map(postRepository.save(
-                mapper.map(post, PostEntity.class)), Post.class);
+                mapper.map(post, Post.class)), PostDTO.class);
     }
 
     @Override
-    public Post updatePost(Post post) {
+    public PostDTO updatePost(PostDTO post) {
         return  mapper.map(postRepository.save(
-                mapper.map(post, PostEntity.class)), Post.class);
+                mapper.map(post, Post.class)), PostDTO.class);
     }
 
     @Override
